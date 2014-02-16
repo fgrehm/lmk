e007f9d64c136e8ab80ad8aee6b0a12e6fb7fd77
Fabio Rehm <fgrehm@gmail.com>
2014-02-16 18:47:08 -0300

    Too much to fit into a commit message
    Closes GH-3 and GH-6

--- lmk.go
+++ lmk.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -9,43 +10,45 @@ import (
 )
 
 func main() {
-	log.Print("Will run CMD")
-	runCmd()
+	// https://gobyexample.com/execing-processes
+	executable, lookErr := exec.LookPath(os.Args[1])
+	if lookErr != nil {
+		log.Fatal(lookErr)
+	}
+	args := os.Args[2:]
 
+	log.Printf("Running %s", os.Args[1:])
+	err := run(executable, args...)
+
+	var icon, msg string
+	if err != nil {
+		icon = "software-update-urgent"
+		msg = fmt.Sprintf("That %s has errored!", os.Args[1:])
+	} else {
+		icon = "emblem-default"
+		msg = fmt.Sprintf("That %s has finished", os.Args[1:])
+	}
 	go func() {
 		for {
-			log.Print("Will notify...")
-			exec.Command("notify-send", "Letting you know...", "That the command has finished").Run()
-			time.Sleep(time.Minute * 1)
+			log.Print("Notifying")
+			exec.Command("notify-send", "-i", icon, "--", "Letting you know...", msg).Run()
+			time.Sleep(time.Second * 30)
 		}
 	}()
 
-	log.Print("Will wait for input")
-
 	in := bufio.NewReader(os.Stdin)
-	_, err := in.ReadString('\n')
+	_, err = in.ReadString('\n')
 	if err != nil {
 		panic(err)
 	}
-
-	log.Print("Leaving...")
 }
 
-func runCmd() {
-	// https://gobyexample.com/execing-processes
-	binary, lookErr := exec.LookPath(os.Args[1])
-	if lookErr != nil {
-		panic(lookErr)
-	}
-	args := os.Args[2:]
-	system(binary, args...)
-}
-
-func system(name string, args ...string) {
-	cmd := exec.Command(name, args...)
+var run = func(executable string, args ...string) error {
+	cmd := exec.Command(executable, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Env = os.Environ()
-	cmd.Run()
+
+	return cmd.Run()
 }
