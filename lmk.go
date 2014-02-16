12860db3c4807f6fa1efe9eeef276508ef4208ac
Fabio Rehm <fgrehm@gmail.com>
2014-02-16 19:35:53 -0300

    Support for custom messages
    Closes GH-5

--- lmk.go
+++ lmk.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"flag"
 	"log"
 	"os"
 	"os/exec"
@@ -10,33 +11,59 @@ import (
 	"time"
 )
 
+var (
+	flagMessage = flag.String("m", "", "")
+
+	defaultMessage = "%s has completed successfully"
+)
+
+var usage = `Usage: lmk [options...] command
+
+Options:
+  -m  Message to display in case of success, defaults to "[command] has completed successfully"
+`
+
 func main() {
-	if len(os.Args) == 1 {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, usage)
+	}
+
+	flag.Parse()
+	if flag.NArg() < 1 {
+		usageAndExit("")
+	}
+
+	if len(flag.Args()) == 0 {
 		log.Fatalf("No command was provided to lmk")
 	}
 
 	// https://gobyexample.com/execing-processes
-	executable, lookErr := exec.LookPath(os.Args[1])
+	flagArgs := flag.Args()
+	executable, lookErr := exec.LookPath(flagArgs[0])
 	if lookErr != nil {
 		log.Fatal(lookErr)
 	}
-	args := os.Args[2:]
+	executableArgs := flagArgs[1:]
 
-	log.Printf("Running %s", os.Args[1:])
-	err := run(executable, args...)
+	log.Printf("Running %s", flagArgs)
+	err := run(executable, executableArgs...)
 
 	var icon, msg string
 	if err != nil {
 		icon = "software-update-urgent"
-		msg = fmt.Sprintf("That %s has errored!", os.Args[1:])
+		msg = fmt.Sprintf("%s has errored!", flagArgs)
 	} else {
 		icon = "emblem-default"
-		msg = fmt.Sprintf("That %s has finished", os.Args[1:])
+		if *flagMessage != "" {
+			msg = *flagMessage
+		} else {
+			msg = fmt.Sprintf(defaultMessage, flagArgs)
+		}
 	}
 	go func() {
 		for {
 			log.Print("Notifying")
-			exec.Command("notify-send", "-i", icon, "--", "Letting you know...", msg).Run()
+			exec.Command("notify-send", "-i", icon, "--", "Heads up!", msg).Run()
 			time.Sleep(time.Second * 30)
 		}
 	}()
@@ -59,4 +86,14 @@ var run = func(executable string, args ...string) error {
 	cmd.Env = os.Environ()
 
 	return cmd.Run()
+}
+
+func usageAndExit(message string) {
+	if message != "" {
+		fmt.Fprintf(os.Stderr, message)
+		fmt.Fprintf(os.Stderr, "\n\n")
+	}
+	flag.Usage()
+	fmt.Fprintf(os.Stderr, "\n")
+	os.Exit(1)
 }
